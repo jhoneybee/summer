21cd209256e44b2cf07852e5fb276954b2a5bcb8
jhonebee <zhangjin19960908@gamil.com>
2021-05-06 10:52:34 +0800

    feat: 拆分代码模块, 让每个组件更加独立

---
+++ packages/theme/src/base-styled.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+export const disable = css`
+    cursor: default;
+    pointer-events: none;
+    background: #f5f5f5;
+    border-color: #d9d9d9;
+    box-shadow: none;
+    color: rgba(0,0,0,.25);
+    text-shadow: none;
+`
