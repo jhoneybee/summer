a471feb97f978b30b5ee71f375386dbcfda38870
jhonebee <zhangjin19960908@gamil.com>
2021-05-18 14:50:16 +0800

    feat: 添加 password 输入框

---
+++ packages/password/src/password.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
+
+import { Input, InputProps } from '@summer/input';
+
+export interface PasswordProps extends InputProps{
+}
+
+export default function Password ({
+    style,
+    ...restProps
+}: PasswordProps) {
+    const [hideValue, setHideValue] = useState<boolean>(true);
+    const Icon = hideValue ? AiOutlineEyeInvisible : AiOutlineEye;
+    return (
+
+            <Input
+                {...restProps}
+                type={hideValue ? 'password' : 'text'}
+                suffix={
+                    <Icon
+                        onClick={() => {
+                            setHideValue(!hideValue);
+                        }}
+                    />
+                }
+            />
+    );
+}
