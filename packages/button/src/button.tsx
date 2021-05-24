58a44dc9b22737976016070a9476a0e32977ed79
jhonebee <zhangjin19960908@gamil.com>
2021-05-24 10:12:17 +0800

    feat: 优化 select 选择器

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -7,14 +7,10 @@ export interface ButtonProps extends Omit<
     HTMLAttributes<HTMLElement>,
     'onClick' | 'inputMode'
 > {
-    // 设置为和父元素同宽
-    block?: boolean
     // 设置为危险按钮
     danger?: boolean
     // 设置按钮是否失效
     disabled?: boolean
-    // 要跳转的地址
-    href?: string
     // 按钮类型
     type?: 'primary' | 'default'
     // 点击事件
@@ -25,7 +21,6 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     block = false,
     danger = false,
     disabled: pDisabled = false,
-    href,
     type = 'default',
     onClick,
     children,
@@ -39,8 +34,8 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     const [disabled, setDisabled] = useState<boolean>(pDisabled)
     return (
         <ButtonStyled
-            ref={ref}
             {...restProps}
+            ref={ref}
             disabled={disabled}
             onClick={(e) => {
                 setDisabled(true);
---
+++ packages/select/src/styled/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SelectOptionStyled = styled.div`
+    padding: 5px 12px;
+    cursor: pointer;
+    :hover {
+        background-color: rgb(245, 245, 245);
+    }
+`
